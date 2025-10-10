b52af580731a9cf8025572ad293cdddd0e62047d
ams32 <46282195-ams32@users.noreply.replit.com>
2025-10-10 09:56:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    return "<h1>Welcome to the WelFore Nutrition App</h1>"
